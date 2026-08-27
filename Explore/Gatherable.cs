26475c8ea8bceeaffe836e211425e6d3daac8de8
Julian Pierce <julian.pierce@gmail.com>
2026-08-27 14:09:35 -0700

    Added spawn rates to gatherables

--- Explore/Gatherable.cs
+++ Explore/Gatherable.cs
@@ -6,6 +6,32 @@ public class Gatherable : MonoBehaviour
     // How many credits this treasure is worth when gathered.
     public int credits = 0;
 
+    // How likely is this gatherable to spawn?
+    // Set to 1f for gatherables that are always there!
+    // Set to at least a bit below 1f for most things, to provide variety!
+    // TBD: Handle violet flowers specially, so there are always 13!
+    public float spawnRate = 0.8f;
+
+    // This gatherable's description, if it has one.
+    [TextArea(10, 30)]
+    public string description = "";
+
+    // + Initialization
+    void Awake()
+    {
+        // Spawn(?)
+        float spawnRoll = Random.Range(0f, 1f);
+        if (spawnRoll < spawnRate)
+        {
+            // Enable.
+            gameObject.SetActive(true);
+        } else {
+            // Disable.
+            gameObject.SetActive(false);
+            return;
+        }
+    }
+
     // Called when another collider enters this collider.
     void OnTriggerEnter2D(Collider2D col)
     {
